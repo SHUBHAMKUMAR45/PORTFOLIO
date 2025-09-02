b358d4f4d90748d563a66d0a7a039cc38b40a118
SHUBHAMKUMAR45 <shubhamkumar2062004@gmail.com>
2025-09-02 15:56:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
+import Footer from './sections/Footer.jsx';
+import Navbar from './sections/Navbar.jsx';
+import Contact from './sections/Contact.jsx';
+import Projects from './sections/Projects.jsx';
+import Experiences from './sections/Experience.jsx';
+
+const App = () => {
+  return (
+    <main className="relative mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experiences />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,216 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Experience",
+    href: "#experience",
+  },
+  {
+    id: 4,
+    name: "Projects",
+    href: "#project",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+
+
+export const myProjects = [
+{
+  title: "Smart Event Scheduler",
+  desc: "Smart Event Scheduler is an AI-powered full-stack scheduling and collaboration platform built using Next.js 15, Tailwind CSS, TypeScript, MongoDB, and NextAuth.js. It offers advanced calendar management with FullCalendar, real-time collaboration via Socket.IO, smart meeting suggestions, and seamless integrations with Google Maps, Zoom, and Teams. Designed for enterprises and teams, it ensures secure scheduling with RBAC and delivers actionable productivity insights.",
+  subdesc:
+    "This project enhanced scheduling efficiency by 45%, reducing meeting conflicts and boosting team collaboration through intelligent time suggestions, real-time updates, and enterprise-grade security.",
+  href: "https://github.com/SHUBHAMKUMAR45/Smart-Event-Scheduler",
+  texture: "/textures/project/Smart.mp4",
+  spotlight: "/assets/spotlight1.png",
+},
+
+  {
+    title: "Zcrum - Project Management Tool",
+    desc: "Zcrum is a full-stack project management web application built using Next.js, Tailwind CSS, Prisma, Neon (PostgreSQL), and Clerk. It provides agile project management features such as Kanban boards, sprint planning, and issue tracking with secure authentication. Designed for teams to collaborate efficiently, Zcrum enhances productivity and reduces delivery delays.",
+    subdesc:
+      "This project improved task efficiency and collaboration by 40%, helping teams streamline workflows and manage sprints effectively with an intuitive interface.",
+    href: "https://github.com/SHUBHAMKUMAR45/Zcrum",
+    texture: "/textures/project/zcrum.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "ClimbWise - AI Career Coaching Platform",
+    desc: "ClimbWise is an AI-powered career coaching platform developed using Next.js, Node.js, PostgreSQL, and the Google Gemini API. It offers personalized job recommendations, resume analysis, and mock interview simulations. The platform helps students and professionals plan their careers with actionable insights driven by AI.",
+    subdesc:
+      "This project reduced server infrastructure costs by 30% through optimization and provided an engaging user experience with responsive design and AI-driven feedback.",
+    href: "https://github.com/SHUBHAMKUMAR45/ClimbWise",
+    texture: "/textures/project/clibwise.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "Shifly - House Selling & Rental App",
+    desc: "Shiftly is a mobile application built using React Native that enables users to easily buy, sell, and rent houses. The app provides property listings with images, pricing, and location details, along with features for secure user authentication, advanced search, and real-time updates. It is designed to make property transactions seamless and accessible from anywhere.",
+    subdesc:
+      "This project streamlined property discovery and rental processes by offering a mobile-first solution with an intuitive UI, secure authentication, and optimized performance for smooth cross-platform usage.",
+    href: "https://github.com/SHUBHAMKUMAR45/Shiftly",
+    texture: "/textures/project/shifly.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "SOEN – MERN-Based AI Chat & Code Assistant",
+    desc: "SOEN is a comprehensive real-time chat application built with the MERN stack (MongoDB, Express.js, React, Node.js). Enhanced with AI support from Google Gemini, the app not only enables instant messaging but also provides coding assistance and code generation. With Redis for caching and Socket.io for smooth communication, SOEN delivers both performance and innovation.",
+    subdesc:
+      "This project combines real-time chat with AI-powered coding support, offering seamless communication, instant AI-generated code snippets, and optimized performance with Redis caching.",
+    href: "https://github.com/SHUBHAMKUMAR45/SOEN",
+    texture: "/textures/project/SOEN.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "AI CODE ANALYST",
+    desc: "The AI Code Analyst project is a web application built using the MERN stack (MongoDB, Express, React, Node.js) that leverages Google Geemi for enhanced analysis. The platform is designed to help developers and programmers assess and optimize their code through automated analysis. By integrating AI-powered insights, the tool analyzes code quality, identifies potential bugs, provides optimization suggestions, and improves code readability.",
+    subdesc:
+      "This project improves the coding workflow by assisting developers with accurate, real-time code reviews and actionable insights, making it easier to maintain high-quality code.",
+    href: "https://github.com/SHUBHAMKUMAR45/ai-code-analyst",
+    texture: "/textures/project/aicode.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "CODE DECK",
+    desc: "Code Deck is an online integrated development environment (IDE) built using React JS. It leverages Styled Components for styling and integrates with the Judge0 CE API via Rapid API for handling code submissions and execution. Axios is used to make API calls, and React Router is employed for seamless routing within the application. This platform allows users to write, test, and submit code directly from their browser.",
+    subdesc:
+      "This project provides a comprehensive online coding environment for developers.",
+    href: "https://github.com/SHUBHAMKUMAR45/code-deck",
+    texture: "/textures/project/CODEDECK.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "RENOVO",
+    desc: "The Renovo Project is a modern web application built using React JS and GSAP (GreenSock Animation Platform), designed to deliver an engaging, interactive user experience with smooth animations. The app focuses on providing dynamic content presentation and seamless transitions, making it visually appealing and intuitive.",
+    subdesc:
+      "The Renovo Project combines the power of React and GSAP to create a visually stunning and highly interactive web experience.",
+    href: "https://github.com/SHUBHAMKUMAR45/RENOVO",
+    texture: "/textures/project/RENOVO.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "GAMING WEBSITE",
+    desc: "The Gaming Website is an interactive platform designed for gamers, offering a sleek and visually engaging interface. Built with React JS and enhanced with GSAP animations, the website provides a smooth, dynamic experience as users explore various games and related content.",
+    subdesc:
+      "This website offers a modern, fun, and interactive platform for discovering and exploring games.",
+    href: "https://github.com/SHUBHAMKUMAR45/Gamming-Website",
+    texture: "/textures/project/GAME2.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "HEALTH PLUS",
+    desc: "The Health Website is an intuitive platform built with React that provides users with comprehensive health information, tools, and resources. It features interactive elements such as symptom checkers, appointment scheduling, and health trackers, designed to empower users to proactively manage their health and well-being.",
+    subdesc:
+      "The website aims to offer a seamless, user-friendly experience for individuals seeking to stay informed and take charge of their health.",
+    href: "https://github.com/SHUBHAMKUMAR45/HEALTH-PLUS",
+    texture: "/textures/project/HEA.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "EMPLOYEE MANAGEMENT SYSTEM",
+    desc: "The Employee Management System is a web application built with React JS and Firebase, designed to streamline HR processes and enhance employee management. The platform offers features such as employee profiles, attendance tracking, and performance evaluations, providing a comprehensive solution for managing human resources.",
+    subdesc:
+      "This application aims to simplify HR operations and improve employee management through a user-friendly, efficient platform.",
+    href: "https://github.com/SHUBHAMKUMAR45/EMS",
+    texture: "/textures/project/ems.mp4",
+    logo: "/assets/logo.png",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "Ecomzy",
+    desc: "A modern, streamlined shopping bag application built with React.js, Redux Toolkit, and Tailwind CSS. React handles the component-based structure and smooth UI interactions. Redux Toolkit streamlines your state management, making it easy to keep track of items in your shopping bag, update quantities, and handle checkout processes. Tailwind CSS ensures your app looks sleek and responsive without spending hours on custom styling.",
+    subdesc:
+      "Built dynamic, visually appealing shopping experience where users can effortlessly browse, add, and remove items in their shopping bag.",
+    href: "https://shopping-bag-livid.vercel.app/",
+    texture: "/textures/project/ecomzyy.mp4",
+    spotlight: "/assets/spotlight1.png",
+  },
+  {
+    title: "Thought Saver",
+    desc: "LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
+    subdesc:
+      "Utilize CSS in your resume to maintain consistency, enhance readability, and create a visually appealing layout while staying true to professional design principles.",
+    href: "https://node-js-project-l0yo.onrender.com/",
+    texture: "/textures/project/tought.mp4",
+    spotlight: "/assets/spotlight2.png",
+  },
+  {
+    title: "Obys Agency",
+    desc: "Implemented modern web development techniques to create a vibrant and interactive user experience.",
+    subdesc:
+      "Demonstrated technical expertise and creativity in web development through the project.",
+    href: "https://shubhamkumar45.github.io/OBYS-AGENCY/",
+    texture: "/textures/project/obye.mp4",
+    spotlight: "/assets/spotlight3.png",
+  },
+  {
+    title: "Document Wallet",
+    desc: "Node.js is your server-side backbone, ensuring scalable, high-performance handling of requests and operations. Express.js simplifies your server-side logic and routing, making it easy to manage HTTP requests and build out the API endpoints for your app.",
+    subdesc:
+      "EJS (Embedded JavaScript) is your templating engine, which dynamically generates HTML with minimal fuss, keeping your front-end and back-end closely connected.",
+    href: "https://doc-wallets.onrender.com/",
+    texture: "/textures/project/docsaveer.mp4",
+    spotlight: "/assets/spotlight4.png",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Full-Stack Developer Intern",
+    job: "CSS EdTech",
+    date: "March 2025 - june 2025",
+    contents: [
+      "During my time at CSSEdTech, I contributed as a full-stack developer with a major focus on frontend development using React.js and backend development using Node.js/Express.js. The role gave me the opportunity to deliver end-to-end features used by real customers in production.",
+
+      "Boosted backend efficiency by 40% by optimizing APIs and implementing server-side caching, which improved scalability under high user loads.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Ensured clean, test-driven, and maintainable code, receiving over 90% positive feedback in code reviews for adherence to best practices and high readability.",
+    ],
+  },
+  {
+    title: "Software Developer Engineer Intern",
+    job: "Docmize- solutions Pvt. Ltd ",
+    date: "May 2025 - Aug 2025",
+    contents: [
+      "Built scalable MERN applications for handling large-scale data ingestion and real-time analysis, ensuring efficient processing and storage in MongoDB.",
+      "Developed backend services with Node.js & Express.js to enable seamless client-to-server communication for data exchange and system control.",
+      "✅ Designed and integrated secure REST APIs with JWT authentication and role-based access, ensuring reliability and compliance",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Enhanced frontend performance in React.js by reducing load times by 35% using lazy loading, code splitting, and caching strategies.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
